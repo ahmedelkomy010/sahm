8df8dcd1069493ab9e4c4fc8ff9484045a04c488
ahmedelkomy010 <ahmedprog877@gmail.com>
2025-05-28 15:58:49 +0300

    jav

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,13 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import { createApp } from 'vue';
+import LabLicenseTable from './components/LabLicenseTable.vue';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const app = createApp({});
+app.component('lab-license-table', LabLicenseTable);
+app.mount('#app');
